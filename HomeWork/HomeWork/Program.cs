83a0bbb5b0e575780e7de8d6497747364e06a3f4
DmytroShu <dimo.shu@gmail.com>
2023-06-10 09:50:31 +0300

    Домашня робота №3

--- HomeWork/HomeWork/Program.cs
+++ HomeWork/HomeWork/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+double res;
+
+while (true)
+{
+    double divider = 2;
+
+    Console.Write("Enter your number: ");
+    var val = double.Parse (Console.ReadLine());
+
+
+
+    if (val > 0 && val <= 3)
+    {
+        Console.WriteLine($"{val} is a prime number");
+    }
+
+    else
+    {  
+        while (val > divider)
+        {
+            res = val % divider;
+
+
+            if (res == 0)
+            {
+                Console.WriteLine($"{val} is NOT a prime number");
+                break;
+            }
+            else if (val - divider == 1)
+            {
+                Console.WriteLine($"{val} is a prime number");
+            }
+            
+            divider++;
+        }
+        
+    }
+}
+
+Console.ReadLine();
+
+
