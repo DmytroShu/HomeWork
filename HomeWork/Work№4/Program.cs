6449ce0d2382f6e81a6494e14384df85f5cffb13
DmytroShu <dimo.shu@gmail.com>
2023-06-12 22:09:39 +0300

    Домашня робота №4

---
+++ HomeWork/Work№4/Program.cs
@@ -0,0 +1,32 @@
+﻿const int a = 5;
+int val = 0;
+bool found = false;
+
+int[] numbers = new int[a];
+
+    Console.WriteLine("Enter your number:");   
+
+    for (int i = 0; i < a; i++)              //We enter the data
+    {
+        numbers[i] = int.Parse(Console.ReadLine()); ;
+    }
+
+    Console.WriteLine("What number are you interested in?");
+    val = int.Parse(Console.ReadLine());
+
+
+    for (int i = 0; i < a; i++)             //Check for a match
+    {
+        if (numbers[i] == val)
+        {
+            Console.WriteLine($"{val} position in the array: {i}");
+            found = true;
+        }       
+    }
+
+    if (found == false)
+    {
+        Console.WriteLine("Number not found");
+    }
+
+Console.WriteLine();
