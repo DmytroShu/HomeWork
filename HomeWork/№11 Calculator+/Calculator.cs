4df86a6aa822c952cb92c43967f1fc9580c163f1
DmytroShu <dimo.shu@gmail.com>
2023-07-31 17:27:26 +0300

    Домашня робота №11 калькулятор

---
+++ HomeWork/№11 Calculator+/Calculator.cs
@@ -0,0 +1,61 @@
+﻿
+namespace _11_Calculator_
+{
+    public static class Calculate
+    {
+        public static string FindAction(string[] substrings)
+        {
+            for (int i = 0; i < substrings.Length; i++)
+            {
+
+                if (!double.TryParse(substrings[i], out double sub) && substrings[i] == "*" || substrings[i] == "/")
+                {
+                    double result = Action(substrings[i - 1], substrings[i + 1], substrings[i]);
+                    substrings[i + 1] = result.ToString();
+                    substrings[i] = "";
+                    substrings[i - 1] = "";
+                }
+            }
+            substrings = Array.FindAll(substrings, item => !string.IsNullOrEmpty(item));
+            for (int i = 0; i < substrings.Length; i++)
+            {
+                if (!double.TryParse(substrings[i], out double sub) && substrings[i] == "+" || substrings[i] == "-")
+                {
+                    string resultate;
+                    try
+                    {
+                        double result = Action(substrings[i - 1], substrings[i + 1], substrings[i]);
+                        substrings[i + 1] = result.ToString();
+                        substrings[i] = "";
+                        substrings[i - 1] = "";
+                    }
+                    catch (Exception ex)
+                    {
+                        return resultate = substrings[i] + substrings[i + 1];
+                    }
+
+
+                }
+            }
+            substrings = Array.FindAll(substrings, item => !string.IsNullOrEmpty(item));
+            return substrings[0];
+        }
+        static double Action(string str1, string str2, string action)
+        {
+            double.TryParse(str1, out double val1);
+            double.TryParse(str2, out double val2);
+            switch (action)
+            {
+                case "*":
+                    return val1 * val2;
+                case "/":
+                    return val1 / val2;
+                case "+":
+                    return val1 + val2;
+                case "-":
+                    return val1 - val2;
+                default: return 0;
+            }
+        }
+    }
+}
---
+++ HomeWork/№11 Calculator+/Program.cs
@@ -0,0 +1,102 @@
+﻿
+using _11_Calculator_;
+using System.Text.RegularExpressions;
+
+namespace Calculator_
+{
+
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    string expression = Console.ReadLine();
+                    Console.WriteLine(Test(expression));
+                }
+                catch (Exception ex)
+                    {
+                    Console.WriteLine("Incorrect expression, please try again");
+                    }
+            }
+
+        }
+        static string Test (string expression)
+        {
+            string[] substrings = Regex.Split(expression, @"([+\-*/()])");
+            if (substrings.Length <= 2) 
+            {
+                return expression;
+            }
+            else
+            {
+                string forTest = Reed(expression);
+                return Test (forTest);
+            }
+        }
+        static string Reed(string str)
+        {
+            int index1 = str.IndexOf('(');
+            int index2 = str.IndexOf(')');
+
+            if (index1 == -1 && index2 == -1)
+            {
+                string[] substrings = Regex.Split(str, @"([+\-*/()])");
+                return Calculate.FindAction(substrings);
+            }
+            else
+            {
+                if (index1 != -1)
+                {
+                    
+                    string part1 = str.Substring(0, index1);
+                    string part2 = str.Substring(index1 + 1);
+                    if (part1 == "")
+                    {
+                        return Reed(part2);
+                    }
+                    else
+                    {
+                        string lastChar = part1.Substring(part1.Length - 1);
+                        if (!double.TryParse(lastChar, out double sub))
+                        {
+                            part1 = part1 + Reed(part2);
+                            return Reed(part1);
+                        }
+                        else
+                        {
+                            part1 = part1 + "*" + Reed(part2);
+                            return Reed(part1);
+                        }
+                    }
+                }
+                else
+                {
+                    string part1 = str.Substring(0, index2);
+                    string part2 = str.Substring(index2 + 1);
+                    if (part2 == "")
+                    {
+                        return Reed(part1);
+                    }
+                    else
+                    {
+                        string firstChar = part2.Substring(0,1);
+                        if (!double.TryParse(firstChar, out double sub))
+                        {
+                            part2 = Reed(part1) + part2;
+                            return part2;
+                        }
+                        else
+                        {
+                            part2 = Reed(part1) + "*" + part2;
+                            return part2;
+                        }
+                    }
+                }
+            }      
+        }
+    }
+}
